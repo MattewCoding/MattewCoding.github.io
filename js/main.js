b4ecaaa34eaf5d76d20ef01ba09184fc73d7d716
Y. Mattew <118850804+MattewCoding@users.noreply.github.com>
2024-11-27 13:57:11 +0100

    [w1,2] JS hack to fake PHP includes
    Rewrote all pages to auto include header, instead of manual addition.

--- js/main.js
+++ js/main.js
@@ -1,3 +1,26 @@
+function loadHeader(selected) {
+    const headerContent = `
+            <a href='./index.html'>
+                <img id='nav-image' src='./media/logo.png' alt='Site logo' title='Reload page' />
+            </a>
+            <nav id='header-nav'>
+                <ul class='header-nav-ul'>
+                    <li class='header-nav-li'${selected === 1 ? " id='current-page'" : ""}> <a href='./week-one-labs.html'>Week 1 Labs</a></li>
+                    <li class='header-nav-li'${selected === 2 ? " id='current-page'" : ""}> <a href='./week-one-report.html'>Week 1 Lecture</a></li>
+                    <li class='header-nav-li'${selected === 3 ? " id='current-page'" : ""}> <a href='./week-two-labs.html'>Week 2 Labs</a></li>
+                    <li class='header-nav-li'${selected === 4 ? " id='current-page'" : ""}> <a href='./week-two-report.html'>Week 2 Lecture</a></li>
+                </ul>
+            </nav>
+            <nav id='header-options'>
+                <ul class='header-nav-ul'>
+                    <li class='header-nav-li' id='darkModeButton'>Dark Mode</li>
+                </ul>
+            </nav>
+    `;
+    document.getElementsByTagName('header')[0].innerHTML = headerContent;
+}
+
+
 function addDarkModeListener() {
     // Get references to the button, light, and dark stylesheets
     const toggleBtn = document.getElementById('darkModeButton');
@@ -8,7 +31,7 @@ function addDarkModeListener() {
     if (localStorage.getItem('darkMode') === 'enabled') {
         darkStylesheet.removeAttribute('disabled');
         lightStylesheet.setAttribute('disabled', 'true');
-        toggleBtn.textContent = 'Switch to Light Mode';
+        toggleBtn.textContent = 'Light Mode';
     }
 
     // Toggle dark mode when button is clicked
@@ -28,4 +51,10 @@ function addDarkModeListener() {
         }
     });
 
+}
+
+function loadPage(selected)
+{
+    loadHeader(selected);
+    addDarkModeListener();
 }
